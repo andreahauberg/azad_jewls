a10720970ea0fff73f6465506ef8187240195054
Andrea <132267442+andreahauberg@users.noreply.github.com>
2023-05-19 17:32:12 +0200

    flere ændringer

---
+++ js/press.js
@@ -0,0 +1,48 @@
+const images = [
+    "img/press1.jpg", "img/press2.jpg", "img/press3.jpg", "img/press4.jpg",
+    "img/press5.jpg", "img/press6.jpg", "img/press7.jpg", "img/press8.jpg"
+  ];
+  
+  let i = 0;
+  
+  function placeImage(x, y) {
+    const nextImage = images[i];
+  
+    const img = document.createElement("img");
+    img.setAttribute("src", nextImage);
+    img.style.left = x + "px";
+    img.style.top = y + "px";
+    img.style.transform = "translate(-50%, -50%) scale(0.1) rotate(" + (Math.random() * 20 - 10) + "deg)";
+  
+    const mainElement = document.querySelector("main");
+    mainElement.appendChild(img); // Tilføj billedet til <main></main>
+  
+    i = i + 1;
+  
+    if (i >= images.length) {
+      i = 0;
+    }
+  }
+  
+  function reloadPage() {
+    location.reload();
+  }
+  
+  window.addEventListener("resize", reloadPage);
+  
+  document.addEventListener("click", function (event) {
+    const mainElement = document.querySelector("main");
+    if (event.target === mainElement) {
+      event.preventDefault();
+      placeImage(event.pageX, event.pageY);
+    }
+  });
+  
+  document.addEventListener("touchend", function (event) {
+    const mainElement = document.querySelector("main");
+    if (event.target === mainElement) {
+      event.preventDefault();
+      placeImage(event.pageX, event.pageY);
+    }
+  });
+  
--- js/script.js
+++ js/script.js
@@ -12,32 +12,3 @@ document.querySelectorAll(".nav_link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
-
-
-//PRESS PAGE
-
-const images = [
-
-  "press1.jpg", "press2.jpg", "press3.jpg", "press4.jpg", 
-  "press5.jpg", "press6.jpg", "press7.jpg", "press8.jpg"
-]
-
-let i = 0 
-
-function placeImage(x, y) {
-
-  const nextImage = images[i]
-
-  const img = document.createElement("img")
-  img.setAttribute("src", nextImage)
-  img.style.left = x + "px"
-  img.style.top = y + "px"
-
-  document.body.appendChild(img)
-
-}
-
-placeImage(500, 400)
-placeImage(400, 500)
-placeImage(600, 300)
-
